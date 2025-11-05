247735f74f8dc02f412993f562cc182e62358320
samyyc <s1myyc@outlook.com>
2025-11-06 00:44:51 +0800

    feat(managed): More method to weapon service

--- managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayer_WeaponServices.cs
+++ managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CPlayer_WeaponServices.cs
@@ -21,6 +21,42 @@ public partial interface CPlayer_WeaponServices {
   public void SelectWeapon(CBasePlayerWeapon weapon);
 
   /// <summary>
+  /// Drop a weapon by slot.
+  /// </summary>
+  /// <param name="slot">The slot to drop the weapon from.</param>
+  public void DropWeaponBySlot( gear_slot_t slot );
+
+  /// <summary>
+  /// Remove a weapon by slot.
+  /// </summary>
+  /// <param name="slot">The slot to remove the weapon from.</param>
+  public void RemoveWeaponBySlot( gear_slot_t slot );
+
+  /// <summary>
+  /// Select a weapon by slot.
+  /// </summary>
+  /// <param name="slot">The slot to select the weapon from.</param>
+  public void SelectWeaponBySlot( gear_slot_t slot );
+
+  /// <summary>
+  /// Drop a weapon by designer name.
+  /// </summary>
+  /// <param name="designerName">The designer name of the weapon to drop.</param>
+  public void DropWeaponByDesignerName( string designerName );
+
+  /// <summary>
+  /// Remove a weapon by designer name.
+  /// </summary>
+  /// <param name="designerName">The designer name of the weapon to remove.</param>
+  public void RemoveWeaponByDesignerName( string designerName );
+
+  /// <summary>
+  /// Select a weapon by designer name.
+  /// </summary>
+  /// <param name="designerName">The designer name of the weapon to select.</param>
+  public void SelectWeaponByDesignerName( string designerName );
+
+  /// <summary>
   /// Drop all weapons with the specified class.
   /// </summary>
   /// <typeparam name="T">The weapon class.</typeparam>
