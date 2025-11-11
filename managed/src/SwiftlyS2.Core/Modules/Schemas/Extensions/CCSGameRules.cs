29db76e2deccb9efaf185077590569c4a5a5fb36
skuzzis <andreieurt@gmail.com>
2025-11-11 21:13:15 +0200

    update(.NET): 8.0 -> 10.0 (first try)

--- managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CCSGameRules.cs
+++ managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CCSGameRules.cs
@@ -13,5 +13,10 @@ public partial interface CCSGameRules
     /// <param name="controller">Player Controller</param>
     public T? FindPickerEntity<T>( CBasePlayerController controller ) where T : ISchemaClass<T>;
 
+    /// <summary>
+    /// Ends the current round with the specified reason after an optional delay
+    /// </summary>
+    /// <param name="reason">The reason for ending the round</param>
+    /// <param name="delay">The delay before ending the round</param>
     public void TerminateRound( RoundEndReason reason, float delay );
 }
